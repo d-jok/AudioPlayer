78aecdc9cb676026557a0b1793b24f3d6c69369e
d-jok <31610302+d-jok@users.noreply.github.com>
2019-08-18 16:47:48 +0300

    Release

---
+++ AudioPlayer/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace AudioPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.fileToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(12, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(161, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Playing:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(247, 354);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(341, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar1.Location = new System.Drawing.Point(164, 382);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(424, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(594, 395);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(43, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(12, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(12, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Previous";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(700, 334);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(56, 104);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(701, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Volume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(762, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "100";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(511, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Playlist:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(514, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 260);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Audio Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AudioPlayer/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+using TagLib;
+
+namespace AudioPlayer
+{
+    public struct AudioInfo
+    {
+        public string Url;
+        public int TimeSec;
+
+        public AudioInfo(string link, int time)
+        {
+            Url = link;
+            TimeSec = time;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer Player;
+        private List<AudioInfo> PlayList = new List<AudioInfo>();
+
+        private int CurrentTrack = 0;
+        private bool isOpen = false;
+        private bool isPlaying = false;      
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar2.Maximum = 100;
+            trackBar2.Value = 50;
+            label3.Text = trackBar2.Value.ToString();
+        }
+
+        ~Form1()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            textBox2.Text = "00:00";
+        }
+
+        private void PlayFile(String url)
+        {
+            textBox1.Text = Path.GetFileName(url);
+            trackBar1.Maximum = PlayList[CurrentTrack].TimeSec;
+
+            Player = new WMPLib.WindowsMediaPlayer();
+
+            Player.URL = url;           
+            Player.controls.play();
+
+            timer1.Start();
+            isPlaying = true;
+        }
+
+        private void Stop()
+        {
+            if (isOpen == true)
+            {
+                isPlaying = false;
+
+                Player.controls.stop();
+                timer1.Stop();
+                textBox2.Text = "00:00";
+                trackBar1.Value = 0;
+            }
+        }
+
+        private void Next()
+        {
+            if (isOpen == true)
+            {
+                Player.controls.stop();
+                timer1.Stop();
+                trackBar1.Value = 0;
+
+                if (CurrentTrack + 1 < PlayList.Count)
+                {
+                    CurrentTrack++;
+                    string url = PlayList[CurrentTrack].Url;
+                    PlayFile(url);
+                }
+                else
+                {
+                    CurrentTrack = 0;
+                    string url = PlayList[CurrentTrack].Url;
+                    PlayFile(url);
+                }
+            }
+        }
+
+        private void Previous()
+        {
+            if (isOpen == true)
+            {
+                Player.controls.stop();
+                timer1.Stop();
+                trackBar1.Value = 0;
+
+                if (CurrentTrack - 1 >= 0)
+                {
+                    CurrentTrack--;
+                    string url = PlayList[CurrentTrack].Url;
+                    PlayFile(url);
+                }
+                else
+                {
+                    CurrentTrack = PlayList.Count - 1;
+                    string url = PlayList[CurrentTrack].Url;
+                    PlayFile(url);
+                }
+            }
+        }
+
+        private void move()
+        {
+            Player.controls.pause();
+            Player.controls.currentPosition = trackBar1.Value;
+            Player.controls.play();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Text = Player.controls.currentPositionString;
+            trackBar1.Value = (int)Player.controls.currentPosition;
+        }       
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                this.Close();
+            }
+        }
+
+        private void Player_MediaError(object pMediaObject)
+        {
+            MessageBox.Show("Cannot play media file.");
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //Play
+        {
+            try
+            {
+                if (isOpen == true && isPlaying == false)
+                {
+                    PlayFile(PlayList[CurrentTrack].Url);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)  //Stop
+        {
+            Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //Next
+        {
+            Next();
+        }
+
+        private void button4_Click(object sender, EventArgs e)  //Previous
+        {
+            Previous();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFiles();            
+        }
+
+        private void OpenFiles()
+        {
+            OpenFileDialog file = new OpenFileDialog()
+            {
+                Multiselect = true,
+                ValidateNames = true
+            };
+
+            TagLib.File File;    //for track (int)time in seconds (using TagLib.dll) 
+            AudioInfo[] Open; 
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                Open = new AudioInfo[file.FileNames.Length];
+                int i = 0;
+
+                try
+                {
+                    foreach (string obj in file.FileNames)
+                    {
+                        File = TagLib.File.Create(obj);
+                        //PlayListAdd(new AudioInfo[] { new AudioInfo(obj, (int)File.Properties.Duration.TotalSeconds) });
+                        Open[i] = new AudioInfo(obj, (int)File.Properties.Duration.TotalSeconds);
+                        i++;
+                    }
+
+                    PlayListAdd(Open);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void PlayListAdd(params AudioInfo[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                PlayList.Add(input[i]);
+                listBox1.Items.Add(Path.GetFileName(input[i].Url));
+            }
+            isOpen = true;
+            PlayFile(PlayList[CurrentTrack].Url);
+            //PlayFile(PlayList[CurrentTrack].Url);   //Add first track to Player.URL;
+            //PlayList.Add(input[0]);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)       //CHECK!!!!
+        {
+            bool clicked = true;
+
+            trackBar1.MouseDown += (s, x) =>
+            {
+                timer1.Stop();
+                clicked = true;
+            };
+
+            trackBar1.MouseUp += (s, x) =>
+            {
+                if (!clicked)
+                    return;
+
+                clicked = false;
+                timer1.Start();
+                move();
+            };
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isOpen == true)
+                {
+                    Player.settings.volume = trackBar2.Value;
+                    label3.Text = trackBar2.Value.ToString();
+                }
+                else
+                    MessageBox.Show("Choose track", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Player.controls.stop();
+
+            CurrentTrack = listBox1.SelectedIndex;
+            PlayFile(PlayList[listBox1.SelectedIndex].Url);
+        }
+    }
+}
